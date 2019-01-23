f4a1b2d92a4bf9536f50fd34a1c68472c0e2c344
Alvi <matuszczakd7895@gmail.com>
2019-01-23 15:13:19 +0100

    Added HTML,SCSS and JS for popup appear/disappear

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
 /* Here goes your JS code */
+
+window.addEventListener("DOMContentLoaded", () => {
+  const btnOpen = document.getElementById("show-popup-form");
+  const btnClose = document.querySelector(".popup__close-btn");
+  const popup = document.querySelector(".popup");
+
+  const showPoup = () => popup.classList.add("is-visible");
+  const hidePopup = () => popup.classList.remove("is-visible");
+
+  btnClose.addEventListener("click", hidePopup);
+  btnOpen.addEventListener("click", showPoup);
+});
